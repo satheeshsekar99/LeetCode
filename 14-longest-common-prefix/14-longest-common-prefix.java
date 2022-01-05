7ef3b3249fbcafcf8765726e9919872c1caa1e81
Satheesh Sekar <52604504+satheeshsekar99@users.noreply.github.com>
2022-01-04 20:44:36 -0500

    Time: 2 ms (38.54%), Space: 38.7 MB (37.78%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.java
+++ 14-longest-common-prefix/14-longest-common-prefix.java
@@ -1,36 +1,37 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        // Scanner sc=new Scanner(System.in);
-        // String[] strs=new String[]{"flower", "flow", "flight"};
-        int i=0;
-        int j=0;
-//        System.out.println("enter strs");
-//        for(i=0; i<3; i++)
-//        {
-//            strs[i]=sc.next();
-//        }
-        int minlength = strs[0].length();
-        for(i=1; i<strs.length; i++)
-        {
-            if(strs[i].length()<minlength)
-            {
-                minlength = strs[i].length();
+        int index = 0;
+        int minLength = findMin(strs);
+
+        while(index < minLength) {
+            if(!checkCharAt(strs, index)) {
+                break;
+            }
+            index++;
+        }
+
+        return strs[0].substring(0,index);
+    }
+        public int findMin(String[] strs) {
+        int minLength = strs[0].length();
+        for(int i =1; i<strs.length;i++) {
+            if(strs[i].length() < minLength) {
+                minLength = strs[i].length();
             }
         }
-        String result="";
-        char current;
-        for(i=0; i<minlength; i++)
-        {
-            current=strs[0].charAt(i);
-            for(j=1; j<strs.length; j++)
-            {
-                if(current!=strs[j].charAt(i))
-                {
-                    return result;
-                }
+        return minLength;
+    }
+
+    public boolean checkCharAt(String[] strs, int index) {
+        char check = strs[0].charAt(index);
+        //boolean flag = true;
+        for(int i =1; i< strs.length; i++) {
+            if(strs[i].charAt(index) != check) {
+				/*flag = false;
+				break;*/
+                return false;
             }
-            result = result+current;
         }
-        return result;
+        return true;
     }
 }
