7cde6e3051923ee7785eb21d2bac0007d2c382d0
Satheesh Sekar <52604504+satheeshsekar99@users.noreply.github.com>
2022-02-03 18:15:45 -0500

    Time: 90 ms (19.16%), Space: 45.1 MB (6.86%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -1,26 +1,30 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         int[] arr = new int[2];
-        int i=0;
         int j=0;
-
-        for (i=0; i<nums.length; i++) {
-            if (j<nums.length) {
-                if(nums[i] + nums[j] == target && i!=j) {
+        for(int i=0; i<nums.length; i++)
+        {
+            if(j<nums.length)
+            {
+                if(nums[i]+nums[j] == target && i!=j)
+                {
                     arr[0] = i;
                     arr[1] = j;
-                    break;
+                    break;                
                 }
-                else {
+                else
+                {
                     i--;
                     j++;
                 }
+                
             }
-            else {
+            else
+            {
                 j=i+1;
             }
+            
         }
         return arr;
-        
     }
 }
